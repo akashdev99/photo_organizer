433384bfd56f6b9ae63a79d3fc22825682f4febb
akashdev99 <akashnandan99@gmail.com>
2025-02-03 05:48:28 +0530

    refatcoring the code

--- organizer.py
+++ organizer.py
@@ -4,62 +4,63 @@ from datetime import datetime
 from os import listdir
 import os
 import shutil
+from pathlib import Path
 
 is_directory_created = {}
-image_path = "/media/akash/ElementX/memories/iphone_automated/"
+image_path = "/media/akash/ElementX/memories/IPHONE_2023_automated/"
 
-def make_folder(directory_path:str, year:int):
-    # Check if the directory exists
-    path = directory_path+str(year)
-    
-    if path in is_directory_created.keys():
+def make_folder(directory_path:str):    
+    if directory_path in is_directory_created.keys():
         print("Yup this exists skipping")
         return 
 
-    if not os.path.exists(path):
-        os.makedirs(path)
-        print(f"Directory '{path}' created successfully.")
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory '{directory_path}' created successfully.")
 
     else:
-        print(f"Directory '{path}' already exists.")
+        print(f"Directory '{directory_path}' already exists.")
+    is_directory_created[directory_path] = True
 
 
 def move_file(source_path:str , directory_path:str, first_level:int, second_level:int=None):
     if second_level:
-        destination_path = directory_path+str(first_level)+"/"+str(second_level)+"/"
+        destination_path = os.path.join(directory_path , str(first_level), f"{str(second_level)}/")
     else:
-        destination_path = directory_path+str(first_level)+"/"
+        destination_path = os.path.join(directory_path , f"{str(first_level)}/")
     shutil.move(source_path , destination_path)
 
+def get_file_type(extension:str):
+    if extension in ["MOV" , "MP4"]:
+        return "videos"
+    else:
+        return "images"
+
+def get_create_date_object(source_path:str):
+    image = Image.open(source_path)
+    exif_data = image.getexif()
+    create_date  = exif_data[306]
+    return datetime.strptime(create_date , "%Y:%m:%d %H:%M:%S")
 
 
 def extract_heic_opener(file_path):
     register_heif_opener()
-    print("no of files to process : ",len(listdir(file_path)))
+    print("Number of files to process : ",len(listdir(file_path)))
     
     for f in listdir(file_path):
-        source_path = file_path+f
-        if "HEIC" in f:
-            image = Image.open(source_path)
-            exif_data = image.getexif()
-            create_date  = exif_data[306]
-            create_date_obj = datetime.strptime(create_date , "%Y:%m:%d %H:%M:%S")
-            make_folder(image_path, create_date_obj.year)
-            make_folder(image_path+str(create_date_obj.year)+"/" , create_date_obj.month)
-
-            move_file(source_path =source_path , directory_path=image_path , first_level=create_date_obj.year , second_level=create_date_obj.month)
-            print("moved file ",image.filename)
-        if "MOV" in f:
-            make_folder(image_path, "videos_mov")
-            move_file(source_path =source_path , directory_path=image_path , first_level="videos_mov")
+        source_path = os.path.join(file_path,f)
+        try:
+            create_date_obj = get_create_date_object(source_path)    
+            make_folder(os.path.join(image_path, str(create_date_obj.year)))
+            make_folder(os.path.join(image_path , str(create_date_obj.year) , str(create_date_obj.month)))  
+            move_file(source_path =source_path , directory_path=image_path , first_level=create_date_obj.year ,second_level=create_date_obj.month)
             print("moved file ",f)
-        if "MP4" in f:
-            make_folder(image_path, "videos_mp4")
-            move_file(source_path =source_path , directory_path=image_path , first_level="videos_mp4")
-            print("moved file ",f)
-        if "JPG" in f or "JPEG" in f:
-            make_folder(image_path, "images_jpg")
-            move_file(source_path =source_path , directory_path=image_path , first_level="images_jpg")
+        except Exception as e :
+            print("failed to move image to by date",e) 
+            extension = Path(source_path).suffix.replace(".","")
+            folder_name = f"{get_file_type(extension)}_{extension}"
+            make_folder(os.path.join(image_path, folder_name))
+            move_file(source_path =source_path , directory_path=image_path , first_level=folder_name)
             print("moved file ",f)
 
-extract_heic_opener("/media/akash/ElementX/memories/iphone_automated/2025/")
+extract_heic_opener("/media/akash/ElementX/memories/IPHONE_2023/")
